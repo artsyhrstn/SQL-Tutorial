bfc7a95e450b26c3c851325ff39cb4ad58d918e7
Hisbi Asyihristani <hisbiasyhrstn@gmail.com>
2024-07-13 23:46:47 +0700

    update

---
+++ SQL_Tutorial_Intermediate.sql
@@ -0,0 +1,26 @@
+/*
+Intermediate
+*/
+
+USE master
+
+INSERT INTO EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holy', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+INSERT INTO EmployeeSalary VALUES
+(1010, NULL, 47000),
+(NULL, 'Salesman', 43000)
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeSalary
+
+-- Membuat JOIN
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+Inner Join SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
