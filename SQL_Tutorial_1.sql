e56e4ed4597fc47db8de7121038e7cff8be5c390
Hisbi Asyihristani <hisbiasyhrstn@gmail.com>
2024-07-13 17:47:56 +0700

    SQL FCC

---
+++ SQL_Tutorial_1.sql
@@ -0,0 +1,77 @@
+-- MEMASUKAN DATA DAN MEMBUAT DATABASE
+CREATE TABLE EmployeeDemographics
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)
+
+CREATE TABLE EmployeeSalary
+(EmployeeID int,
+JobTitle varchar(50),
+Salary int
+)
+
+-- MEMASUKAN DATA KE DALAM TABEL
+INSERT INTO EmployeeDemographics VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
+
+INSERT INTO EmployeeSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations'
+, 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
+
+-- MACAM-MACAM SELECT
+
+SELECT * -- Untuk memilih semua
+FROM EmployeeDemographics
+
+SELECT FirstName
+FROM EmployeeDemographics
+
+SELECT FirstName, LastName
+FROM EmployeeDemographics
+
+-- Top, Distinct, Count, As, Max, Min, Avg
+SELECT TOP 5 * 
+FROM EmployeeDemographics
+
+SELECT DISTINCT(Gender) -- Menampilkan hanya karakter unik, ID akan menampilkan semua, sedangkan gender hanya akan menampilkan 2 (Male dan Female)
+FROM EmployeeDemographics
+
+SELECT COUNT(LastName) AS LastNameCount-- Menampilkan jumlah dari baris
+FROM EmployeeDemographics
+
+-- Memilih nilai maksimal, minimal, dan rata-rata
+SELECT MAX(Salary)
+FROM EmployeeSalary
+
+SELECT MIN(Salary)
+FROM EmployeeSalary
+
+SELECT AVG(Salary)
+FROM EmployeeSalary
+
+SELECT *
+FROM EmployeeSalary
+
+USE SQLTutorial
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeSalary
