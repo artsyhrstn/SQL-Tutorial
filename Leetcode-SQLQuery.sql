3058867518687faaa569dedf2da87ca32026ca5e
Hisbi <hisbiasyhrstn@gmail.com>
2024-08-15 14:30:08 +0700

    update

--- Leetcode-SQLQuery.sql
+++ Leetcode-SQLQuery.sql
@@ -39,3 +39,11 @@ LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
 SELECT product_name, year, price
 FROM product, sales
 WHERE sales.product_id=product.product_id
+
+-- Soal 8
+SELECT v.customer_id, COUNT(v.visit_id) as count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY customer_id
